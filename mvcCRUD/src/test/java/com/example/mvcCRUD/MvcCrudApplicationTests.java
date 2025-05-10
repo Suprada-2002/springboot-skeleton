1e715c3ae898fff1e26371e372f48d836e39d74c
Suprada-2002 <supradatripathy789@gmail.com>
2025-05-10 23:13:13 +0530

    Initial push

---
+++ mvcCRUD/src/test/java/com/example/mvcCRUD/MvcCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
